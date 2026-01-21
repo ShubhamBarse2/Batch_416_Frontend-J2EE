ca6b1850b626a8d6f405017a8ca26f5297e6eb31
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-01-21 16:04:26 +0530

    Add files via upload

---
+++ SpringBootDay1/src/main/java/com/tka/SpringBootDay1/SpringBootDay1Application.java
@@ -0,0 +1,16 @@
+package com.tka.SpringBootDay1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay1Application {
+
+	
+	public static void main(String[] args) {
+		
+		System.err.println("Application is started..!");
+		SpringApplication.run(SpringBootDay1Application.class, args);
+	}
+
+}
